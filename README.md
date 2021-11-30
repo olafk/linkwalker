520cd9d845eb2c9025459302562bd85a7c7d9f29
Olaf Kock <olaf.kock+github@liferay.com>
2021-11-30 14:52:56 +0100

    built for 7.4, changed from Dynamic Incluce to ThemeContributor

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ links with a HEAD request and marks them pink and -striked through-
 if the link appears invalid.
 
 It's based on Jan Verweij's suggestion, slightly adapted and turned
-into a deployable plugin through Liferay's Dynamic Include mechanism. 
+into a deployable plugin through Liferay's Theme Contributor mechanism. 
 
 ## How to build
 
 This plugin was built in a Liferay Workspace's `modules` folder for 
-Liferay DXP 7.2 
+Liferay DXP 7.4 
 
 ## Limitations
 
@@ -19,3 +19,9 @@ false positives, e.g. they might show as invalid even though they are
 valid. This can be mitigated by specifically checking for relative links
 or known hostnames - if it becomes a problem in any system, feel free to
 send pullrequests with appropriate workarounds.
+
+Due to [LPS-104516](https://issues.liferay.com/browse/LPS-104516), 
+Widget Pages need to be handled differently than Content Pages. Be aware
+that _any change_ in the DOM that cause Content Pages to be detected 
+might trigger LPS-104516 _with catastrophic consequences_ to the content
+on your page (read the issue).
---
+++ src/main/resources/META-INF/resources/js/linkwalker.js
@@ -0,0 +1,61 @@
+/**
+ * This component adds JS code to the page that checks some links for validity
+ * and marks them in case they're not valid (e.g. the HEAD request returns a code
+ * >399 or an error. Note that there are false positives in case the targetted
+ * site does not allow access through CORS rules.
+ * 
+ * Based on a code suggestion by Jan Verweij, slightly edited on error handling,
+ * and turned into a DynamicInclude to minimize interaction.
+ * 
+ * Due to LPS-104516 Widget Pages need to be handled separately: This code scans
+ * only code from within portlets' bodies on a Widget Page. For Content Pages, 
+ * the whole #main-content block will be tested.
+ * 
+ * @author Olaf Kock, Jan Verweij
+ */
+
+
+function linkwalkerUrlExists(element, callback) {
+    var href=element.getAttribute("href");
+    if (href != null && href != ";" && ( href.startsWith("http") || href.startsWith("/"))) { 
+    	var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() { 
+			if (xhr.readyState === 4) {
+				callback(xhr.status < 400, 'xhr'+xhr.status); 
+			} 
+		};
+		xhr.onerror = function() { 
+			callback(false,'xhrerror'); 
+		}
+		xhr.open('HEAD', href); 
+		xhr.send(); 
+	} 
+}
+
+
+var visitAndMarkLinks = function() {
+	var selector = ".portlet-body a"; // safe fallback for widget pages due to LPS-104516
+
+	if(document.getElementsByClassName("portlet-layout row").length > 0) {
+		console.log("Linkchecking a Widget Page");
+	} else {
+		console.log("Linkchecking a Content Page");
+		selector = "div#main-content a";
+	}
+	
+	var contentPageLinks = document.querySelectorAll(selector);
+
+	contentPageLinks.forEach(function( element ) { 
+        linkwalkerUrlExists(element, function(exists, hint) {
+            console.log('%s (%s) exists?', element.innerHTML, element.getAttribute("href"), exists); 
+            if (!exists) {
+                element.classList.add("brokenlink"); 
+                element.classList.add(hint); 
+            } 
+        }); 
+    }); 
+}
+
+            
+Liferay.on("endNavigate", visitAndMarkLinks);
+Liferay.on("allPortletsReady", visitAndMarkLinks);
